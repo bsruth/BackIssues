a85ab8a7670b589be214391b10c0b62721f6ac06
Brian Ruth <bsruth@LittleMac.local>
2016-01-21 08:31:28 -0700

    CHANGED: Refactored the db backup code

--- BackIssues/src/main/java/brian/ruth/backissues/BackupDBUtilities.java
+++ BackIssues/src/main/java/brian/ruth/backissues/BackupDBUtilities.java
@@ -1,7 +1,6 @@
 package brian.ruth.backissues;
 
 import android.os.Environment;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,39 +15,29 @@ public class BackupDBUtilities {
     public static boolean BackupToSDCard(String privateDBPath) {
         File sd = Environment.getExternalStorageDirectory();
         File data = Environment.getDataDirectory();
-        String backupDBPath = BackIssuesDBHelper.DATABASE_NAME;
         File currentDB = new File(data, privateDBPath);
-        File backupDB = new File(sd, backupDBPath);
-        try {
-            FileChannel source=null;
-            FileChannel destination=null;
-            source = new FileInputStream(currentDB).getChannel();
-            destination = new FileOutputStream(backupDB).getChannel();
-            destination.transferFrom(source, 0, source.size());
-            source.close();
-            destination.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
+        String dbFileName = currentDB.getName();
+        File backupDB = new File(sd, dbFileName);
+        return CopyDatabase(currentDB, backupDB);
     }
 
     public static boolean RestoreFromSDCard(String privateDBPath ) {
         File sd = Environment.getExternalStorageDirectory();
         File data = Environment.getDataDirectory();
-        FileChannel source=null;
-        FileChannel destination=null;
-        String backupDBPath = BackIssuesDBHelper.DATABASE_NAME;
         File currentDB = new File(data, privateDBPath);
-        File backupDB = new File(sd, backupDBPath);
+        String dbFileName = currentDB.getName();
+        File backupDB = new File(sd, dbFileName);
+        return CopyDatabase(backupDB, currentDB);
+    }
+
+    private static boolean CopyDatabase(File source, File dest) {
         try {
-            source = new FileInputStream(backupDB).getChannel();
-            destination = new FileOutputStream(currentDB).getChannel();
-            destination.transferFrom(source, 0, source.size());
-            source.close();
-            destination.close();
-        } catch(IOException e) {
+            FileChannel sourceChannel = new FileInputStream(source).getChannel();
+            FileChannel destChannel = new FileOutputStream(dest).getChannel();
+            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+            sourceChannel.close();
+            destChannel.close();
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
