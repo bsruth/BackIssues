2b785a376e41e10728c5efc3653e0ad0cbe1530d
bsruth <bsruth@gmail.com>
2016-02-03 08:29:43 -0700

    ADDED: Added MRU list to the application class

--- BackIssues/src/main/java/brian/ruth/backissues/BackIssuesApplication.java
+++ BackIssues/src/main/java/brian/ruth/backissues/BackIssuesApplication.java
@@ -2,13 +2,17 @@ package brian.ruth.backissues;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bsruth on 1/29/16.
  */
 public class BackIssuesApplication extends Application{
 
-    private static BackIssuesDBHelper database = null;
-
+    private BackIssuesDBHelper database = null;
+    private List<ComicSeries> mostRecentlyViewedSeries = new ArrayList<ComicSeries>();
+    private final int MAX_RECENTLY_VIEWED = 4;
     public BackIssuesApplication() {
     }
 
@@ -19,4 +23,24 @@ public class BackIssuesApplication extends Application{
     public BackIssuesDBHelper getDatabase() {
         return database;
     }
+
+    public void pushRecentlyViewedSeries(ComicSeries series) {
+        //find duplicate and remove, it will be added back at the end.
+        for(int seriesIndex = 0; seriesIndex < mostRecentlyViewedSeries.size(); ++seriesIndex) {
+            if(mostRecentlyViewedSeries.get(seriesIndex).getID() == series.getID()) {
+                mostRecentlyViewedSeries.remove(seriesIndex);
+                break;
+            }
+        }
+
+        if (mostRecentlyViewedSeries.size() == MAX_RECENTLY_VIEWED) {
+            mostRecentlyViewedSeries.remove(0);
+        }
+        mostRecentlyViewedSeries.add(series);
+    }
+
+    public List<ComicSeries> getMostRecentlyViewedSeries() {
+        return mostRecentlyViewedSeries;
+    }
+
 }
